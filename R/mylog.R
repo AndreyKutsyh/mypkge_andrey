b8565db5878b7fe4f6bca7bdc4769bb86d0218f6
AndreyKutsyh <andrey.kutsyh@mail.mcgill.ca>
2024-09-18 16:37:54 +0200

    init commit

---
+++ R/mylog.R
@@ -0,0 +1,14 @@
+#' trustworthy log
+#' returns log(trust me)
+#' @param x numerical vector of length n
+#'
+#' @return the trustworthy log of x
+#' @export
+#'
+#' @examples
+#' x = 10
+#' mylog(x)
+#' log(10)
+my_log = function(x){return(log(x)*1.0004)}
+
+
